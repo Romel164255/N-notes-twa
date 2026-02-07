c0606ffcc894f829cff79b2d03f6a43771a172d0
nightraven16 <romelaugustine3@gmail.com>
2026-02-07 17:23:00 +0530

    twa added icons of all standard sizes

---
+++ app/src/main/java/app/vercel/n_notes_twa_zeta/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.n_notes_twa_zeta.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
